3e9525c43da674a7b885fe5c399de17558b9abe5
salamPhard <salamfadilu@gmail.com>
2022-07-01 10:04:22 -0700

    rand and if statement edit

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,7 +2,7 @@
 import random
 number = random.randint(-10000, 10000)
 if number < 0:
-    rem = number % 10
+    rem = number % -10
 else:
     rem = number % 10
 print("Last digit of", number, "is", rem, end=' ')
